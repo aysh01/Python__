09033c87720e72d879b7cdab043edaf4b4bf0e4c
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2024-01-11 15:20:43 +0530

    Create example.py

---
+++ practicals/example.py
@@ -0,0 +1,32 @@
+a=[23,23,1231,124124]
+
+# # append()
+b=["mango",'apple','cherry']
+
+# a.append(43)
+# print(a)
+
+# # copy()
+# print(a.copy())
+
+# clear()
+# print(a.clear())
+
+# count()
+# print(a.count(23))
+
+# extend()
+# a.extend(b)
+# print(a)
+
+# x=a.index(1231)
+# print(x)
+
+# a.insert(1,'abc')
+# print(a)
+
+# a.pop()
+# print(a)
+
+# b.remove("mango")
+# print(b)
