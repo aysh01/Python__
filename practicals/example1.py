ead66790bb8e45711b866308ce63afbd5ae2e6cb
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2024-01-12 12:57:14 +0530

    Create example1.py

---
+++ practicals/example1.py
@@ -0,0 +1,15 @@
+
+animals={'reptiles':'snake','mamals':'whale','amphibians':'frogs'}
+
+#print (animals.keys())
+print(animals.values())
+
+#write a prog. to accept number & find the sum. 
+
+a=[]
+for i in range(3):
+    x=float(input("Enter any Number: "))
+    a.append(x)
+print(a)
+#x=sum(a)
+print(sum(a))
