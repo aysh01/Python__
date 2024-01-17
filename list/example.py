9b6406a3a105bd03c9b48b44d392fe8bd6320918
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2024-01-17 15:05:50 +0530

    Create example.py

---
+++ list/example.py
@@ -0,0 +1,16 @@
+a=[23123,'123123','abc']
+
+# for i in range(3):
+#     x=input('Enter Input: ')
+#     a.append(x)
+# # 23break
+# print(a)
+
+# del(), modify() & remove() elements in python..
+a[-1]='good...' #modify values in the particular index..
+print('MOdified Array..',a)
+a.insert(2,'what the.....') #insert() whatever value in the given said index value...
+print(a)
+del a[1] #delete specified index position element from the list..
+a.remove('good...') #remove specified element from the list..
+print("remove..",a) 
